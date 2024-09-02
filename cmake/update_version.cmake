be2670b7ff5b025eb66b3ad23250790bccafa092
Bezuglyj Vitaly <bevidm@yandex.ru>
2024-09-03 00:10:08 +0300

    Add auto-update version

---
+++ cmake/update_version.cmake
@@ -0,0 +1,23 @@
+# update_version.cmake
+
+file(READ "${CMAKE_SOURCE_DIR}/.version.txt" CURRENT_VERSION)
+string(STRIP "${CURRENT_VERSION}" CURRENT_VERSION)
+
+string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" _ ${CURRENT_VERSION})
+set(MAJOR_VERSION "${CMAKE_MATCH_1}")
+set(MINOR_VERSION "${CMAKE_MATCH_2}")
+set(PATCH_VERSION "${CMAKE_MATCH_3}")
+
+# Optionally, update the version only if requested
+if(UPDATE_VERSION)
+    math(EXPR PATCH_VERSION "${PATCH_VERSION} + 1")
+
+    set(PROJECT_VERSION "${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}")
+
+    file(WRITE "${CMAKE_SOURCE_DIR}/.version.txt" "${PROJECT_VERSION}")
+
+    message(STATUS "Updated to version ${PROJECT_VERSION}")
+else()
+    set(PROJECT_VERSION "${CURRENT_VERSION}")
+    message(STATUS "Current version is ${PROJECT_VERSION}")
+endif()
