0915ab0344121ed4549e1c3797f3e69adaaf03ae
vladimir.razdrogin <vladimir.razdrogin@aliexress.ru>
2024-12-02 18:17:49 +0300

    gitignore

---
+++ cmake-build-debug/CMakeFiles/CMakeScratch/TryCompile-E4yMSN/CMakeFiles/cmTC_3f27b.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_3f27b.dir/CMakeCCompilerABI.c.o"
+  "CMakeFiles/cmTC_3f27b.dir/CMakeCCompilerABI.c.o.d"
+  "cmTC_3f27b"
+  "cmTC_3f27b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_3f27b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CMakeScratch/TryCompile-E4yMSN/CMakeFiles/cmTC_3f27b.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cmTC_3f27b.
+# This may be replaced when dependencies are built.
